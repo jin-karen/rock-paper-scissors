21c75afcb1b0fd13d78a8e950e571a2492d09d36
Karen Jin <102393842+jin-karen@users.noreply.github.com>
2022-09-15 04:25:38 -0400

    added animation
    Added a start and end/play again screens, added animation to the start and game screen, added skip button  (still working on usability)

--- gameplay.js
+++ gameplay.js
@@ -49,45 +49,119 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const rockBtn = document.querySelector("#rock");
-const paperBtn = document.querySelector("#paper");
-const scissorsBtn = document.querySelector("#scissors");
+const startBtn = document.querySelector("#startBtn");
+const title = document.querySelector(".title");
+const subtitle = document.querySelector(".subtitle");
+const context = document.querySelectorAll("#context");
+const playAgainBtn = document.querySelector("#playAgainBtn");
+const startScreen = document.querySelector("#startScreen");
+const gameScreen = document.querySelector("#gameScreen");
+const endScreen = document.querySelector("#endScreen");
+const choiceBtns = document.querySelectorAll('.choiceBtn')
+
 let playerScore = 0;
 let computerScore = 0;
 let roundsPlayed = 0;
 let winner = 5;
 
-const body = document.querySelector("body");
-const choiceBtns = document.querySelectorAll(".choiceBtn");
 const results = document.querySelector("#results");
-const scores = document.querySelector("#scores");
 const player = document.querySelector("#player");
 const computer = document.querySelector("#computer");
 const rounds = document.querySelector("#rounds");
+const winnerResult = document.querySelector("#winnerResult");
+const winnerImg = document.querySelector("#winnerImg");
 
-rockBtn.addEventListener('click',() => {
-    const computerSelection = getComputerChoice();
-    let playerSelection = "rock";
-    playRound(playerSelection,computerSelection);
+const skip = document.querySelector(".skip");
+const contextLine1 = document.querySelector("#contextLine1");
+const contextLine2 = document.querySelector("#contextLine2");
+const contextLine3 = document.querySelector("#contextLine3");
+const contextLine4 = document.querySelector("#contextLine4");
+
+startBtn.addEventListener('click',() => {
+    startScreenAnimation();
+    window.setTimeout(() => {
+        startScreen.classList.toggle('hidden');
+        gameScreen.classList.toggle('hidden');        
+    }, 1000);
+    gameScreenAnimation();
 });
-paperBtn.addEventListener('click',() => {
-    const computerSelection = getComputerChoice();
-    let playerSelection = "paper";
-    playRound(playerSelection,computerSelection);
-});
-scissorsBtn.addEventListener('click',() => {
-    const computerSelection = getComputerChoice();
-    let playerSelection = "scissors";
-    playRound(playerSelection,computerSelection);
+
+function startScreenAnimation() {
+    title.setAttribute('style','transform: translate(-50%, -145%); transition: all 1s;');
+    subtitle.setAttribute('style','opacity: 0; transition: all 1s;');
+}
+
+function gameScreenAnimation() {
+    skip.classList.toggle('conceal');
+    contextLine1.classList.toggle('appear');
+    window.setTimeout(() => {    
+        contextLine2.classList.toggle('conceal');
+        contextLine2.classList.toggle('appear');
+    }, 3000);         
+    window.setTimeout(() => {
+        contextLine3.classList.toggle('conceal');
+        contextLine3.classList.toggle('appear');
+    }, 6000);   
+    window.setTimeout(() => {    
+        contextLine4.classList.toggle('conceal');
+        contextLine4.classList.toggle('appear');
+    }, 9000);   
+    window.setTimeout(() => {    
+        skip.classList.toggle('hidden');
+        const conceal = document.querySelectorAll('.conceal');
+        for (const items of conceal) {
+            items.classList.toggle('conceal');
+        }
+        const appear = document.querySelectorAll('.appear');
+        for (const items of appear) {
+            items.classList.toggle('appear');
+        }
+    }, 12000);  
+}
+
+skip.addEventListener('click',skipAnimation);
+
+function skipAnimation() {
+    const conceal = document.querySelectorAll('.conceal');
+    for (const items of conceal) {
+        items.classList.toggle('conceal');
+    }
+    context.stop(true,true);
+    //This doesn't work, can't get javascript to skip queue
+}
+
+choiceBtns.forEach((button) => {
+    button.addEventListener('click',() => {
+        const computerSelection = getComputerChoice();
+        const img = button.querySelector("img");
+        const playerSelection = img.alt;
+        playRound(playerSelection,computerSelection);
+    });
+})
+
+playAgainBtn.addEventListener('click',() => {
+    endScreen.classList.toggle('hidden');
+    gameScreen.classList.toggle('hidden');
 });
 
 function endGame() {
+    gameScreen.classList.toggle('hidden');
+    endScreen.classList.toggle('hidden');
     if (playerScore === winner) {
-        results.textContent = "Congratulations, You Win!";
+        winnerResult.textContent = "Congratulations, You Win!";
+        winnerImg.setAttribute('src', 'img/camper.png');
     } else {
-        results.textContent = "Sorry, You Lose!";
-    }        
+        winnerResult.textContent = "Sorry, You Lose!";
+        winnerImg.setAttribute('src', 'img/bear.png');
+    }    
+    reset()
+}
+
+function reset() {
     playerScore = 0;
     computerScore = 0;
     roundsPlayed = 0;
+    results.textContent = "Camper vs. Bear";
+    player.textContent = "";//`${playerName} Score:  ${playerScore}`; 
+    computer.textContent = "";//`${computerName} Score: ${computerScore}`;
 }
