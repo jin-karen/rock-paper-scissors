62c214e8ab4ec4e2f1696babbe731ea5aa80fd2a
Karen Jin <102393842+jin-karen@users.noreply.github.com>
2022-09-15 22:42:36 -0400

    update files
    Added documentation and improved organization

--- gameplay.js
+++ gameplay.js
@@ -1,9 +1,117 @@
-//player synonymous with camper; computer synonymous with bear - can be changed with story context
+//based on story context and gameplay- values can be changed
 let playerName = "Camper";
 let computerName = "Bear";
+let playerScore = 0;
+let computerScore = 0;
+let winner = 5;
+
+//audio sounds
 let btnSound = document.querySelector('#btnSound');
 let winSound = document.querySelector('#winSound');
 let loseSound = document.querySelector('#loseSound');
+
+//different pages of the game
+const startScreen = document.querySelector("#startScreen");
+const gameScreen = document.querySelector("#gameScreen");
+const helpScreen = document.querySelector("#helpScreen");
+const endScreen = document.querySelector("#endScreen");
+
+//buttons used to navigate pages and play game
+const startBtn = document.querySelector("#startBtn");
+const helpBtn = document.querySelector("#helpBtn");
+const exitBtn = document.querySelector("#exitBtn");
+const playAgainBtn = document.querySelector("#playAgainBtn");
+const choiceBtns = document.querySelectorAll('.choiceBtn');
+
+//animation and skip animation
+const skipBtn = document.querySelector("#skipBtn");
+const contextLine1 = document.querySelector("#contextLine1");
+const contextLine2 = document.querySelector("#contextLine2");
+const contextLine3 = document.querySelector("#contextLine3");
+const contextLine4 = document.querySelector("#contextLine4");
+
+//display for score and game results
+const results = document.querySelector("#results");
+const player = document.querySelector("#player");
+const computer = document.querySelector("#computer");
+const winnerResult = document.querySelector("#winnerResult");
+const winnerImg = document.querySelector("#winnerImg");
+
+//rounds played not visible but can be changed to view
+let roundsPlayed = 0;
+const rounds = document.querySelector("#rounds");
+
+//start button - navigate from start to game screen
+startBtn.addEventListener('click',() => {
+    btnSound.play()
+    startScreenAnimation();
+    window.setTimeout(() => {
+        startScreen.classList.toggle('hidden');
+        gameScreen.classList.toggle('hidden');        
+    }, 1000);
+    gameScreenAnimation();
+});
+
+//animation
+function startScreenAnimation() {
+    const title = document.querySelector("#title");
+    const subtitle = document.querySelector("#subtitle");
+    title.setAttribute('style','transform: translate(-50%, -145%); transition: transform 1s;');
+    subtitle.setAttribute('style','opacity: 0; transition: opacity 1s;');
+}
+
+function gameScreenAnimation() {
+    skipBtn.classList.toggle('conceal');
+    contextLine1.classList.toggle('appear');
+    //delayed animation to reveal story lines one at a time
+    contextLine2.setAttribute('style','animation-delay: 3s;');
+    contextLine2.classList.toggle('appear');
+    contextLine3.setAttribute('style','animation-delay: 6s;');
+    contextLine3.classList.toggle('appear');
+    contextLine4.setAttribute('style','animation-delay: 9s;');
+    contextLine4.classList.toggle('appear');
+    window.setTimeout(() => {    
+        //run only if animation was not skipped to avoid revealing skip button
+        if (!skipBtn.classList.contains('hidden')) {
+            endAnimation();
+        }
+    }, 12000);
+}
+
+//skip button - skips animation on game screen
+skipBtn.addEventListener('click',()=>{
+    btnSound.play();
+    endAnimation();
+});
+
+//ends animation and reveals game 
+function endAnimation() {
+    skipBtn.classList.toggle('hidden');
+    //reveals rest of game by toggling off conceal
+    const conceal = document.querySelectorAll('.conceal');
+    for (const items of conceal) {
+        items.classList.toggle('conceal');
+    }
+    //toggling off animation from context for 'play again' games
+    const appear = document.querySelectorAll('.appear');
+    for (const items of appear) {
+        items.classList.toggle('appear');
+    }
+    contextLine2.setAttribute('style','animation-delay: 0s;');
+    contextLine3.setAttribute('style','animation-delay: 0s;');
+    contextLine4.setAttribute('style','animation-delay: 0s;');
+}
+
+//gameplay 
+choiceBtns.forEach((button) => {
+    button.addEventListener('click',() => {
+        btnSound.play()
+        const computerSelection = getComputerChoice();
+        const img = button.querySelector("img");
+        const playerSelection = img.alt;
+        playRound(playerSelection,computerSelection);
+    });
+})
 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
@@ -52,116 +160,6 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const startBtn = document.querySelector("#startBtn");
-const title = document.querySelector(".title");
-const subtitle = document.querySelector(".subtitle");
-const helpBtn = document.querySelector("#helpBtn");
-const exitBtn = document.querySelector("#exitBtn");
-const playAgainBtn = document.querySelector("#playAgainBtn");
-const startScreen = document.querySelector("#startScreen");
-const gameScreen = document.querySelector("#gameScreen");
-const helpScreen = document.querySelector("#helpScreen");
-const endScreen = document.querySelector("#endScreen");
-const choiceBtns = document.querySelectorAll('.choiceBtn');
-
-let playerScore = 0;
-let computerScore = 0;
-let winner = 5;
-
-const results = document.querySelector("#results");
-const player = document.querySelector("#player");
-const computer = document.querySelector("#computer");
-const winnerResult = document.querySelector("#winnerResult");
-const winnerImg = document.querySelector("#winnerImg");
-
-const skipBtn = document.querySelector(".skipBtn");
-const contextLine1 = document.querySelector("#contextLine1");
-const contextLine2 = document.querySelector("#contextLine2");
-const contextLine3 = document.querySelector("#contextLine3");
-const contextLine4 = document.querySelector("#contextLine4");
-
-let roundsPlayed = 0;
-const rounds = document.querySelector("#rounds");
-
-startBtn.addEventListener('click',() => {
-    btnSound.play()
-    startScreenAnimation();
-    window.setTimeout(() => {
-        startScreen.classList.toggle('hidden');
-        gameScreen.classList.toggle('hidden');        
-    }, 1000);
-    gameScreenAnimation();
-});
-
-function startScreenAnimation() {
-    title.setAttribute('style','transform: translate(-50%, -145%); transition: transform 1s;');
-    subtitle.setAttribute('style','opacity: 0; transition: opacity 1s;');
-}
-
-function gameScreenAnimation() {
-    skipBtn.classList.toggle('conceal');
-    contextLine1.classList.toggle('appear');
-    contextLine2.setAttribute('style','animation-delay: 3s;');
-    contextLine2.classList.toggle('appear');
-    contextLine3.setAttribute('style','animation-delay: 6s;');
-    contextLine3.classList.toggle('appear');
-    contextLine4.setAttribute('style','animation-delay: 9s;');
-    contextLine4.classList.toggle('appear');
-    window.setTimeout(() => {    
-        if (!skipBtn.classList.contains('hidden')) {
-            endAnimation();
-        }
-    }, 12000);
-}
-
-skipBtn.addEventListener('click',()=>{
-    btnSound.play();
-    endAnimation();
-});
-
-function endAnimation() {
-    skipBtn.classList.toggle('hidden');
-    const conceal = document.querySelectorAll('.conceal');
-    for (const items of conceal) {
-        items.classList.toggle('conceal');
-    }
-    const appear = document.querySelectorAll('.appear');
-    for (const items of appear) {
-        items.classList.toggle('appear');
-    }
-    contextLine2.setAttribute('style','animation-delay: 0s;');
-    contextLine3.setAttribute('style','animation-delay: 0s;');
-    contextLine4.setAttribute('style','animation-delay: 0s;');
-}
-
-choiceBtns.forEach((button) => {
-    button.addEventListener('click',() => {
-        btnSound.play()
-        const computerSelection = getComputerChoice();
-        const img = button.querySelector("img");
-        const playerSelection = img.alt;
-        playRound(playerSelection,computerSelection);
-    });
-})
-
-helpBtn.addEventListener('click',() => {
-    btnSound.play()
-    gameScreen.classList.toggle('hidden');
-    helpScreen.classList.toggle('hidden');
-});
-
-exitBtn.addEventListener('click',() => {
-    btnSound.play()
-    helpScreen.classList.toggle('hidden');
-    gameScreen.classList.toggle('hidden');
-});
-
-playAgainBtn.addEventListener('click',() => {
-    btnSound.play()
-    endScreen.classList.toggle('hidden');
-    gameScreen.classList.toggle('hidden');
-});
-
 function endGame() {
     gameScreen.classList.toggle('hidden');
     endScreen.classList.toggle('hidden');
@@ -185,4 +183,25 @@ function reset() {
     player.textContent = "";//`${playerName} Score:  ${playerScore}`; 
     computer.textContent = "";//`${computerName} Score: ${computerScore}`;
 }
+
+//help button - navigate from game to help screen
+helpBtn.addEventListener('click',() => {
+    btnSound.play()
+    gameScreen.classList.toggle('hidden');
+    helpScreen.classList.toggle('hidden');
+});
+
+//exit button - navigate from help to game screen
+exitBtn.addEventListener('click',() => {
+    btnSound.play()
+    helpScreen.classList.toggle('hidden');
+    gameScreen.classList.toggle('hidden');
+});
+
+//play again button - navigate from end to game screen
+playAgainBtn.addEventListener('click',() => {
+    btnSound.play()
+    endScreen.classList.toggle('hidden');
+    gameScreen.classList.toggle('hidden');
+});
 
